2827bd6de6b16a2ccbd7183bd315e996ed7a1072
IdeoG <ideogaib@gmail.com>
2018-08-23 12:37:59 +0300

    Добавил первичный функционал просмотра инвентаря: собирает информацию о всех подобранных предметах и переключение между предметами в инвентарями для их просмотра

---
+++ Assets/_GuardianLight/Scripts/UI/Inventory/InventoryCanvasBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryCanvasBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject _inventoryArrows;
+    [SerializeField] private GameObject _oddIventoryItems;
+    [SerializeField] private GameObject _evenIventoryItems;
+
+    private InventoryItemsBehaviour _oddItemsBehaviour;
+    private InventoryItemsBehaviour _evenItemsBehaviour;
+    
+
+    private void Awake()
+    {
+        _oddItemsBehaviour = _oddIventoryItems.GetComponent<InventoryItemsBehaviour>();
+        _evenItemsBehaviour = _evenIventoryItems.GetComponent<InventoryItemsBehaviour>();
+    }
+
+    private void OnEnable()
+    {
+        /** TODO: OnEnable
+         * 1. Обращаемся к инвентарю
+         * 2. Получаем все подобранные предметы из инвентаря
+         * 3.1. Если количество предметов четное, то используем четный паттерн
+         * 3.1. Если количество предметов нечетное, то используем нечетный паттерн
+         * 4. Заполняем места предметы графического инвентаря спрайтами и цветами
+         */
+
+        var inventory = InventorySystem.Instance;
+
+        var items = inventory.GetItems();
+
+        
+        if (items.Count % 2 == 0 && items.Count < 5)
+        {
+            SetEvenPattern(items);
+        }
+        else
+        {
+            SetOddPattern(items);
+        }
+
+        _inventoryArrows.SetActive(items.Count > 5);
+    }
+
+    private void SetEvenPattern(List<Item> items)
+    {
+        _oddIventoryItems.SetActive(false);
+        _evenIventoryItems.SetActive(true);
+        _evenItemsBehaviour.ClearImages();
+        
+        var len = items.Count;
+        for (var ind = 0; ind < len; ind++)
+        {
+            _evenItemsBehaviour.SetImage(ind, items[ind].Sprite, Color.white);
+            _evenItemsBehaviour.SetCurrentDescription(items[ind].Name);
+        }
+    }
+
+    private void SetOddPattern(List<Item> items)
+    {
+        _oddIventoryItems.SetActive(true);
+        _evenIventoryItems.SetActive(false);
+        _oddItemsBehaviour.ClearImages();
+
+        var len = items.Count;
+        for (var ind = 0; ind < len; ind++)
+        {
+            _oddItemsBehaviour.SetImage(ind, items[ind].Sprite, Color.white);
+            _oddItemsBehaviour.SetCurrentDescription(items[ind].Name);
+        }
+    }
+}
---
+++ Assets/_GuardianLight/Scripts/UI/Inventory/InventoryItemsBehaviour.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemsBehaviour : MonoBehaviour
+{
+    [Header("Описание текущего предмета")] [SerializeField]
+    private Text _itemDescription;
+
+    [Header("Подсветка текущего предмета")] [SerializeField]
+    private RectTransform _itemLighting;
+
+    [Header("Пулл предметов")] [SerializeField]
+    private List<GameObject> _items;
+
+    private List<RectTransform> _rectTransforms = new List<RectTransform>();
+    private List<Image> _images = new List<Image>();
+
+    private IDisposable _leftArrowPress;
+    private IDisposable _rightArrowPress;
+
+    private int _inventoryPosition;
+
+    private void Awake()
+    {
+        foreach (var item in _items)
+        {
+            _rectTransforms.Add(item.GetComponent<RectTransform>());
+            _images.Add(item.GetComponent<Image>());
+        }
+    }
+
+    private void OnEnable()
+    {
+        _leftArrowPress = InputSystem.Instance.KeyLeftArrowPressed
+            .Subscribe(_ =>
+            {
+                Debug.Log(string.Format("LeftArrowPressed"));
+                OnLeftArrowPressed();
+            })
+            .AddTo(this);
+
+        _rightArrowPress = InputSystem.Instance.KeyRightArrowPressed
+            .Subscribe(_ => OnRightArrowPressed())
+            .AddTo(this);
+    }
+
+    private void OnDisable()
+    {
+        _leftArrowPress.Dispose();
+        _rightArrowPress.Dispose();
+    }
+
+    private void OnLeftArrowPressed()
+    {
+        if (_inventoryPosition % 2 == 0)
+        {
+            if (_inventoryPosition >= _items.Count - 2) return;
+            _inventoryPosition += 2;
+        }
+        else
+        {
+            _inventoryPosition = Mathf.Clamp(_inventoryPosition - 2, 0, _items.Count);
+        }
+
+        SetCurrentLighting(_inventoryPosition);
+    }
+
+    private void OnRightArrowPressed()
+    {
+        if (_inventoryPosition % 2 != 0)
+        {
+            if (_inventoryPosition >= _items.Count - 1) return;
+
+            _inventoryPosition = _inventoryPosition + 2;
+        }
+        else
+        {
+            if (_inventoryPosition == 0)
+            {
+                _inventoryPosition = 1;
+            }
+            else
+            {
+                _inventoryPosition = Mathf.Clamp(_inventoryPosition - 2, 0, _items.Count);
+            }
+        }
+
+        SetCurrentLighting(_inventoryPosition);
+    }
+
+    private void SetCurrentLighting(int position)
+    {
+        _itemLighting.SetPositionAndRotation(_rectTransforms[position].position, _rectTransforms[position].rotation);
+    }
+
+    public void SetImage(int position, Sprite sprite, Color color)
+    {
+        _images[position].sprite = sprite;
+        _images[position].color = color;
+    }
+
+    public void SetCurrentDescription(string text)
+    {
+        _itemDescription.text = text;
+    }
+
+    public void ClearImages()
+    {
+        foreach (var image in _images)
+        {
+            image.sprite = null;
+            image.color = new Color(0, 0, 0, 0);
+        }
+    }
+}
