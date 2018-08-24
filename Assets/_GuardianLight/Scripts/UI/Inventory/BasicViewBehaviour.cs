88b2f8d6609cf0a3a69fb9237ec2a9e1956750e8
IdeoG <ideogaib@gmail.com>
2018-08-24 10:20:48 +0300

    Почти доделал инвентарь. Основные работы сделаны.

---
+++ Assets/_GuardianLight/Scripts/UI/Inventory/BasicViewBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicViewBehaviour : MonoBehaviour {
+
+	[SerializeField] private GameObject _inventoryArrows;
+	[SerializeField] private GameObject _oddIventoryItems;
+	[SerializeField] private GameObject _evenIventoryItems;
+
+	private InventoryItemsBehaviour _itemsBehaviour;
+	private List<Item> _items;
+
+	private void SetEvenPattern(List<Item> items)
+	{
+		_oddIventoryItems.SetActive(false);
+		_evenIventoryItems.SetActive(true);
+        
+		_itemsBehaviour = _evenIventoryItems.GetComponent<InventoryItemsBehaviour>();
+		_itemsBehaviour.SetItems(items);
+        
+	}
+
+	private void SetOddPattern(List<Item> items)
+	{
+		_oddIventoryItems.SetActive(true);
+		_evenIventoryItems.SetActive(false);
+
+		_itemsBehaviour = _oddIventoryItems.GetComponent<InventoryItemsBehaviour>();
+		_itemsBehaviour.SetItems(items);
+	}
+
+	private void OnEnable()
+	{
+		var items = InventorySystem.Instance.GetItems();
+		_items = items;
+		
+		_inventoryArrows.SetActive(items.Count > 5);
+		if (items.Count % 2 == 0 && items.Count < 5)
+		{
+			SetEvenPattern(items);
+		}
+		else
+		{
+			SetOddPattern(items);
+		}
+
+	}
+
+	public Item GetCurrentItem()
+	{
+		var ind = _itemsBehaviour.GetCurrentPosition();
+		return _items[ind];
+	}
+}
---
+++ Assets/_GuardianLight/Scripts/UI/Inventory/InspectViewBehaviour.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InspectViewBehaviour : MonoBehaviour
+{
+    [SerializeField] private Text _itemDescription;
+    [SerializeField] private Vector3 _eulerRotation;
+    [SerializeField] private Transform _pivotTransform;
+
+    [Header("Просмотр объекта")]
+    [SerializeField] private float _sensetivityX;
+    [SerializeField] private float _sensetivityY;
+    
+    private GameObject _item;
+    private Transform _itemTransform;
+
+    private IDisposable _keyLeftArrowPressed;
+    private IDisposable _keyRightArrowPressed;
+    private IDisposable _keyUpArrowPressed;
+    private IDisposable _keyDownArrowPressed;
+    
+    
+    public void SetItem(Item item)
+    {
+        _itemDescription.text = item.Description;
+
+        _item = Instantiate(item.Prefab);
+        _item.layer = LayerMask.NameToLayer("UI");
+
+        _itemTransform = _item.transform;
+        _itemTransform.parent = _pivotTransform;
+        _itemTransform.position = _pivotTransform.position;
+        _itemTransform.rotation = Quaternion.Euler(_eulerRotation);
+        _itemTransform.localScale = _pivotTransform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        _keyUpArrowPressed = InputSystem.Instance.KeyUpArrowPressed
+            .Subscribe(_ => OnKeyUpArrowPressed())
+            .AddTo(this);
+        
+        _keyDownArrowPressed = InputSystem.Instance.KeyDownArrowPressed
+            .Subscribe(_ => OnKeyDownArrowPressed())
+            .AddTo(this);
+        
+        _keyLeftArrowPressed = InputSystem.Instance.KeyLeftArrowPressed
+            .Subscribe(_ => OnKeyLeftArrowPressed())
+            .AddTo(this);
+        
+        _keyRightArrowPressed = InputSystem.Instance.KeyRightArrowPressed
+            .Subscribe(_ => OnKeyRightArrowPressed())
+            .AddTo(this);
+    }
+
+    private void OnKeyUpArrowPressed()
+    {
+        // TODO: Добавить ограничения на (min,max) scale
+        _itemTransform.localScale += _sensetivityY * Vector3.one;
+    }
+
+    
+    private void OnKeyDownArrowPressed()
+    {
+        // TODO: Добавить ограничения на (min,max) scale
+        _itemTransform.localScale -= _sensetivityY * Vector3.one;
+    }
+
+    private void OnKeyLeftArrowPressed()
+    {
+        _itemTransform.localRotation *= Quaternion.Euler(new Vector3(0, -_sensetivityX * 1f, 0));
+    }
+
+    private void OnKeyRightArrowPressed()
+    {
+        _itemTransform.localRotation *= Quaternion.Euler(new Vector3(0, _sensetivityX * 1f, 0));
+    }
+    
+    private void OnDisable()
+    {
+        _keyUpArrowPressed.Dispose();
+        _keyDownArrowPressed.Dispose();
+        _keyLeftArrowPressed.Dispose();
+        _keyRightArrowPressed.Dispose();
+        
+        Destroy(_item);
+    }
+}
