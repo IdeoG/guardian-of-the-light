315813c2dedc782a550046fd42455bd8382964cc
IdeoG <ideogaib@gmail.com>
2018-08-27 00:18:54 +0300

    improve -> добавил Здоровье и Грибы

---
+++ Assets/_GuardianLight/Scripts/Base/Components/BaseHealthAction.cs
@@ -0,0 +1,47 @@
+using System;
+using UniRx;
+using UnityEngine;
+
+public abstract class BaseHealthAction : MonoBehaviour
+{
+    private const string RequiredTag = "Player";
+    private IDisposable _keyActionPressed;
+    private IDisposable _keyExtraActionPressed;
+
+    protected Animator Animator;
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var isPlayer = other.tag.Equals(RequiredTag);
+
+        if (!isPlayer) return;
+
+        _keyActionPressed = InputSystem.Instance.KeyActionPressed
+            .Subscribe(_ => OnKeyActionPressed(other.gameObject.GetComponent<Health>()))
+            .AddTo(this);
+        
+        _keyExtraActionPressed = InputSystem.Instance.KeyExtraActionPressed
+            .Subscribe(_ => OnKeyExtraActionPressed(other.gameObject.GetComponent<Health>()))
+            .AddTo(this);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var isPlayer = other.tag.Equals(RequiredTag);
+
+        if (!isPlayer) return;
+
+        _keyActionPressed.Dispose();
+    }
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+
+        gameObject.GetComponent<BoxCollider>().isTrigger = true;
+    }
+    
+    protected abstract void OnKeyActionPressed(Health playerHealth);
+    protected abstract void OnKeyExtraActionPressed(Health playerHealth);
+}
---
+++ Assets/_GuardianLight/Scripts/Base/Components/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] public float MaxHealth;
+    [SerializeField] public float Value;
+
+
+    public bool CanEnhance()
+    {
+        return Value < MaxHealth;
+    }
+
+    public bool CanReduce()
+    {
+        return Value > 0;
+    }
+
+    public void Reduce(float value)
+    {
+        Value = Mathf.Clamp(Value - value, 0, MaxHealth);
+    }
+
+    public void Enhance(float value)
+    {
+        Value = Mathf.Clamp(Value + value, 0, MaxHealth);
+    }
+
+    private void Awake()
+    {
+        Value = Value > 0 ? Value : MaxHealth;
+    }
+}
