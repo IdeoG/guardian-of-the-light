94fa9d8e117128824d1a873cb65b24169c64761b
IdeoG <ideogaib@gmail.com>
2018-09-13 01:33:13 +0300

    Добавил компоненту изменения света персонажа

---
+++ Assets/_Guardian of the Light/Scripts/Base/Health/Lighting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+    [SerializeField] private Light _light;
+    [SerializeField] private float _lightMaxRange = 8.5f;
+    private Health _health;
+
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+
+    }
+
+    private void Start()
+    {
+        _health.ReactivePercent.ObserveEveryValueChanged(x => x.Value)
+            .Subscribe(x => OnHealthChanged(x));
+        
+        OnHealthChanged(_health.ReactivePercent.Value);
+    }
+
+    private void OnHealthChanged(float percent)
+    {
+        _light.range = percent * _lightMaxRange;
+    }
+}
