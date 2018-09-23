b62d561245dc48cede3a47a116b5571aee267d47
IdeoG <ideogaib@gmail.com>
2018-09-23 03:47:34 +0300

    Улучшения по коду: 1. Добавил минимальное значение радиуса свечения героя 2. Добавил изменение цвета тумана в режиме редактора и игры 3. Добавил пресет HDR Color для Глаз игрока

---
+++ Assets/_Guardian of the Light/Scripts/Systems/GameController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[ExecuteInEditMode] 
+public class GameController : MonoBehaviour
+{
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = (int) _vSyncCount;
+        RenderSettings.fogColor = _fogColor;
+    }
+
+    private void OnValidate()
+    {
+        RenderSettings.fogColor = _fogColor;
+    }
+
+    #region half private vars
+
+    [SerializeField] private VSyncType _vSyncCount = VSyncType.EveryVSync60Fps;
+    [SerializeField] private Color _fogColor;
+
+        #endregion
+}
+
+public enum VSyncType
+{
+    EveryVSync60Fps = 1,
+    EverySecondVSync30Fps = 2,
+    NoVSync = 0
+}
