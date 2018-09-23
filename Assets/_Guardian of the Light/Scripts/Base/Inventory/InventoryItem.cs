c5d60098a0f574e8effa6d28e30c88db777cfb92
IdeoG <ideogaib@gmail.com>
2018-09-23 03:12:54 +0300

    Добавил Multiline для инспектора Inventory Description

--- Assets/_Guardian of the Light/Scripts/Base/Inventory/InventoryItem.cs
+++ Assets/_Guardian of the Light/Scripts/Base/Inventory/InventoryItem.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class InventoryItem
 {
     public string Name;
-    public string Description;
+    [Multiline] public string Description;
     public GameObject Prefab2D;
     public GameObject Prefab3D;
     public bool IsTook;
