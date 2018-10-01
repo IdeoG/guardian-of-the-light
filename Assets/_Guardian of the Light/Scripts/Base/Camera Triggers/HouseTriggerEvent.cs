22f5211641af96285393dfe5b3518491911f7d5f
IdeoG <ideogaib@gmail.com>
2018-10-01 22:19:11 +0300

    небольшой фикс

--- Assets/_Guardian of the Light/Scripts/Base/Camera Triggers/HouseTriggerEvent.cs
+++ Assets/_Guardian of the Light/Scripts/Base/Camera Triggers/HouseTriggerEvent.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 
 public class HouseTriggerEvent : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCameraBase _houseCameraSystem;
-    [SerializeField] private CinemachineVirtualCameraBase _playerFreeCamera;
+    [SerializeField] private CinemachineVirtualCameraBase _enterCamera;
+    [SerializeField] private CinemachineVirtualCameraBase _exitCamera;
 
     public void OnHouseEnter()
     {
-        _houseCameraSystem.Priority = 11;
-        _playerFreeCamera.Priority = 10;
+        _enterCamera.Priority = 11;
+        _exitCamera.Priority = 10;
+
+        _enterCamera.enabled = true;
+        _exitCamera.enabled = false;
     }
 
     public void OnHouseExit()
     {
-        _houseCameraSystem.Priority = 10;
-        _playerFreeCamera.Priority = 11;
+        _enterCamera.Priority = 10;
+        _exitCamera.Priority = 11;
+        
+        _enterCamera.enabled = false;
+        _exitCamera.enabled = true;
     }
 }
