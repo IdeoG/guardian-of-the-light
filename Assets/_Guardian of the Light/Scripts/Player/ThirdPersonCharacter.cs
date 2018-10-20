2c9b1d6a3af5c6a6df6698ed46a4d8a97e24e2d8
IdeoG <ideogaib@gmail.com>
2018-10-20 23:53:14 +0300

    small fixes

--- Assets/_Guardian of the Light/Scripts/Player/ThirdPersonCharacter.cs
+++ Assets/_Guardian of the Light/Scripts/Player/ThirdPersonCharacter.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -5,14 +6,14 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class ThirdPersonCharacter : MonoBehaviour
 {
-    private float m_GravityMultiplier = 2f;
+    private const float GravityMultiplier = 2f;
     private float m_GroundCheckDistance = 10f;
-    private float m_JumpPower = 5f;
+    private const float JumpPower = 5f;
     [SerializeField] private float m_AnimSpeedMultiplier = 1.5f;
     [SerializeField] private float m_MoveSpeedMultiplier = 2.1f;
-    private float m_StationaryTurnSpeed = 180;
-    private float m_MovingTurnSpeed = 360;
-    private float m_RunCycleLegOffset = 0.2f;
+    private const float StationaryTurnSpeed = 180;
+    private const float MovingTurnSpeed = 360;
+    private const float RunCycleLegOffset = 0.2f;
     
     private const float KHalf = 0.5f;
     private Animator m_Animator;
@@ -44,7 +45,7 @@ public class ThirdPersonCharacter : MonoBehaviour
 
     public void Move(Vector3 move, bool crouch, bool jump)
     {
-        if (move.magnitude > 1f) move.Normalize();
+        if (Math.Abs(move.magnitude - 1f) > 0.01f) move.Normalize();
         
         move = transform.InverseTransformDirection(move);
         move = Vector3.ProjectOnPlane(move, m_GroundNormal);
@@ -70,8 +71,8 @@ public class ThirdPersonCharacter : MonoBehaviour
         if (m_IsGrounded && crouch)
         {
             if (m_Crouching) return;
-            m_Capsule.height = m_Capsule.height / 2f;
-            m_Capsule.center = m_Capsule.center / 2f;
+            m_Capsule.height = m_Capsule.height * 0.5f;
+            m_Capsule.center = m_Capsule.center * 0.5f;
             m_Crouching = true;
         }
         else
@@ -105,14 +106,13 @@ public class ThirdPersonCharacter : MonoBehaviour
 
     private void UpdateAnimator(Vector3 move)
     {
-        // update the animator parameters
         m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
         m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
         m_Animator.SetBool("Crouch", m_Crouching);
         m_Animator.SetBool("OnGround", m_IsGrounded);
         if (!m_IsGrounded) m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
 
-        var runCycle = Mathf.Repeat(m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+        var runCycle = Mathf.Repeat(m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + RunCycleLegOffset, 1);
         var jumpLeg = (runCycle < KHalf ? 1 : -1) * m_ForwardAmount;
         if (m_IsGrounded) m_Animator.SetFloat("JumpLeg", jumpLeg);
         
@@ -124,7 +124,7 @@ public class ThirdPersonCharacter : MonoBehaviour
 
     private void HandleAirborneMovement()
     {
-        var extraGravityForce = Physics.gravity * m_GravityMultiplier - Physics.gravity;
+        var extraGravityForce = Physics.gravity * GravityMultiplier - Physics.gravity;
         
         m_Rigidbody.AddForce(extraGravityForce);
         m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
@@ -135,7 +135,7 @@ public class ThirdPersonCharacter : MonoBehaviour
     {
         if (!jump || crouch || !m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded")) return;
         
-        m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
+        m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, JumpPower, m_Rigidbody.velocity.z);
         m_IsGrounded = false;
         m_Animator.applyRootMotion = false;
         m_GroundCheckDistance = 0.1f;
@@ -143,14 +143,15 @@ public class ThirdPersonCharacter : MonoBehaviour
 
     private void ApplyExtraTurnRotation()
     {
-        var turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+        var turnSpeed = Mathf.Lerp(StationaryTurnSpeed, MovingTurnSpeed, m_ForwardAmount);
         transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
     }
 
 
     public void OnAnimatorMove()
     {
-        if (!m_IsGrounded || !(Time.deltaTime > 0)) return;
+        var isPlaying = Time.deltaTime > 0;
+        if (!m_IsGrounded || !isPlaying) return;
         
         var v = m_Animator.deltaPosition * m_MoveSpeedMultiplier / Time.deltaTime;
         v.y = m_Rigidbody.velocity.y;
