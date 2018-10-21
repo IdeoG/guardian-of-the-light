86f4d90c91e3f567c2ebd3cbdd7b78186976a6fa
IdeoG <ideogaib@gmail.com>
2018-10-21 23:20:17 +0300

    Исправил ошибку с панелью

--- Assets/_Guardian of the Light/Scripts/GameLogic/Hint/SawmillCristal.cs
+++ Assets/_Guardian of the Light/Scripts/GameLogic/Hint/SawmillCristal.cs
@@ -6,18 +6,20 @@ namespace _Guardian_of_the_Light.Scripts.UI.Hint
 {
     public class SawmillCristal : UiHint
     {
-        [Header("Panel")] 
-        [SerializeField] private float dampingTimeSec;
-        [Range(0f,1f)] [SerializeField] private float _panelCristalDampingStart;
+        [Header("Cristals")]
+        [SerializeField] private float _roofDelayTimeSec;
+        [SerializeField] private float _roofDampingDurationSec;
+        [SerializeField] private float _panelDampingDurationSec;
         [SerializeField] private MeshRenderer _mainCristal;
-        [SerializeField] private Light _mainCristalLight;
+        [SerializeField] private List<Light> _roofLights;
+        [SerializeField] private List<MeshRenderer> _roofCristals;
         [SerializeField] private List<MeshRenderer> _panelCristals;
         
-        [Header("Main Cristal Color")] 
-        [SerializeField] private float _maxLightIntensity;
-        [SerializeField] private AnimationCurve _mainCristalСolorCurve;
-        [ColorUsage(false,true)] [SerializeField] private Color _maxMainCristalColor;
-        [ColorUsage(false,true)] [SerializeField] private Color _minMainCristalColor;
+        [Header("Roof Cristal Color")] 
+        [SerializeField] private AnimationCurve _roofCristalСolorCurve;
+        [SerializeField] private float _maxIntensityRoofLight;
+        [ColorUsage(false,true)] [SerializeField] private Color _maxRoofCristalColor;
+        [ColorUsage(false,true)] [SerializeField] private Color _minRoofCristalColor;
         
         [Header("Panel Cristal Color")] 
         [SerializeField] private AnimationCurve _panelCristalСolorCurve;
@@ -26,40 +28,44 @@ namespace _Guardian_of_the_Light.Scripts.UI.Hint
         
         public override void DestroyItem()
         {
+            _mainCristal.gameObject.SetActive(false);
             StartCoroutine(FadePanel());
         }
 
         private IEnumerator FadePanel()
         {
-            var overallTimeMs = 0f;
-
-            while (overallTimeMs < dampingTimeSec * (1 + _panelCristalDampingStart))
+            var overallTimeSec = 0f;
+            var maxOverallTimeSec = Mathf.Max(_roofDampingDurationSec + _roofDelayTimeSec, _panelDampingDurationSec);
+            
+            while (overallTimeSec < maxOverallTimeSec )
             {
                 yield return null;
 
-                if (overallTimeMs < dampingTimeSec )
+                if (overallTimeSec < _panelDampingDurationSec )
                 {
-                    var percent = overallTimeMs / dampingTimeSec;
-                    _mainCristal.material.SetColor("_EmissionColor", 
-                        Color.Lerp(_minMainCristalColor, _maxMainCristalColor, 1 - _mainCristalСolorCurve.Evaluate(percent)));
-                    _mainCristalLight.intensity = _maxLightIntensity * (1 - _mainCristalСolorCurve.Evaluate(percent));
-                } 
-                else if (_mainCristal.gameObject.activeSelf)
-                {
-                    _mainCristal.gameObject.SetActive(false);
-                }
-
-                if (overallTimeMs > dampingTimeSec * _panelCristalDampingStart)
-                {
-                    var percent = (overallTimeMs - dampingTimeSec * _panelCristalDampingStart ) / dampingTimeSec;
-                    
+                    var percent = overallTimeSec / _panelDampingDurationSec;
                     foreach (var cristal in _panelCristals)
                     {
                         cristal.material.SetColor("_Color0", 
-                            Color.Lerp(_minPanelCristalColor, _maxPanelCristalColor, 1 - _panelCristalСolorCurve.Evaluate(percent)));
+                            Color.Lerp(_minRoofCristalColor, _maxRoofCristalColor, 1 - _panelCristalСolorCurve.Evaluate(percent)));
                     }
                 }
-                overallTimeMs += Time.deltaTime;      
+
+                if (overallTimeSec > _roofDelayTimeSec)
+                {
+                    var percent = (overallTimeSec - _roofDelayTimeSec) / _roofDampingDurationSec;
+                    foreach (var cristal in _roofCristals)
+                    {
+                        cristal.material.SetColor("_EmissionColor", 
+                            Color.Lerp(_minPanelCristalColor, _maxPanelCristalColor, 1 - _roofCristalСolorCurve.Evaluate(percent)));
+                    }
+
+                    foreach (var roofLight in _roofLights)
+                    {
+                        roofLight.intensity = _maxIntensityRoofLight * (1 - _roofCristalСolorCurve.Evaluate(percent));
+                    }
+                }
+                overallTimeSec += Time.deltaTime;      
             }
             
             Destroy(gameObject);
