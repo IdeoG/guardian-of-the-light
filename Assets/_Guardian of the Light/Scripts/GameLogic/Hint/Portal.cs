8db22505c87a9d4a06b3aaa79ca0e3d3527b3f39
IdeoG <ideogaib@gmail.com>
2018-10-27 19:40:20 +0300

    набросал базовый функционал

---
+++ Assets/_Guardian of the Light/Scripts/GameLogic/Hint/Portal.cs
@@ -0,0 +1,17 @@
+namespace _Guardian_of_the_Light.Scripts.GameLogic.Hint
+{
+    public class Portal : UiHint
+    {
+        public override void DestroyItem()
+        {
+            
+        }
+    
+        private void HideVignette() {}
+        
+        private void LoadScene() {}
+        
+        private void ShowVignette() {}
+        
+    }
+}
