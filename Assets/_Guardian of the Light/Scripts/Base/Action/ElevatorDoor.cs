6f198acbf5f5067437cf32e1378def5f2e2e478e
IdeoG <ideogaib@gmail.com>
2018-11-02 01:00:46 +0300

    Написал скрипт для открывания двери лифта

---
+++ Assets/_Guardian of the Light/Scripts/Base/Action/ElevatorDoor.cs
@@ -0,0 +1,51 @@
+using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace _Guardian_of_the_Light.Scripts.Base.Action
+{
+    public class ElevatorDoor : BaseAction
+    {
+        protected override void OnKeyActionPressedDown()
+        {
+            InputSystem.Instance.IsAnimationPlaying = true;
+            GameManagerSystem.Instance.Player.GetComponent<ThirdPersonUserControl>().LockInput = true;
+            
+            _animator.SetBool("OpenDoor", !_animator.GetBool("OpenDoor"));
+        }
+
+        private void Awake()
+        {
+            _animator = GetComponentInParent<Animator>();
+            var triggers = _animator.GetBehaviours<ObservableStateMachineTrigger>();
+
+            foreach (var trigger in triggers)
+            {
+                trigger.OnStateUpdateAsObservable()
+                    .Subscribe(info =>
+                        {
+                            if (_closingDoorStateHash.Equals(info.StateInfo.shortNameHash) && Math.Abs(info.StateInfo.normalizedTime - 1.0f) < 1e-2)
+                            {
+                                OnDoorStateExit();
+                            }
+                            else if (_openingDoorStateHash.Equals(info.StateInfo.shortNameHash) && Math.Abs(info.StateInfo.normalizedTime - 1.0f) < 1e-2)
+                            {
+                                OnDoorStateExit();
+                            }
+                        },
+                        () => Debug.Log($"ElevatorDoor: OnStateUpdateAsObservable -> onCompleted"));
+            }
+        }
+
+        private void OnDoorStateExit()
+        {
+            InputSystem.Instance.IsAnimationPlaying = false;
+            GameManagerSystem.Instance.Player.GetComponent<ThirdPersonUserControl>().LockInput = false;
+        }
+
+        private Animator _animator;
+        private readonly int _openingDoorStateHash = Animator.StringToHash("Opening Door");
+        private readonly int _closingDoorStateHash = Animator.StringToHash("Closing Door");
+    }
+}
