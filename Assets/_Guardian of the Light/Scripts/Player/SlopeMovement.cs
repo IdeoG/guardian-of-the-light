60639694050ba95bb279f2bf3856c38d75050987
IdeoG <ideogaib@gmail.com>
2018-11-13 00:19:01 +0300

    Думаю, что пока закончил с этим исследованием

--- Assets/_Guardian of the Light/Scripts/Player/SlopeMovement.cs
+++ Assets/_Guardian of the Light/Scripts/Player/SlopeMovement.cs
@@ -9,19 +9,21 @@ namespace _Guardian_of_the_Light.Scripts.Player
         private void OnCollisionStay(Collision other)
         {
             var isStair = other.collider.tag.Equals("Stair");
-            var isMoving = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")).magnitude > 0.1f;
+            var moveAmount = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")).magnitude * 0.707f;
+            var isMoving = moveAmount > 0.1f;
+            
             if (!isStair || !isMoving)
                 return;
-            
+
             var cachedPosition = _transform.position;
             foreach (var contactPoint in other.contacts)
             {
                 var diff = contactPoint.point - cachedPosition;
-                var isForwardLook = Mathf.Abs(Vector3.Angle(diff.With(y: 0), _transform.rotation * Vector3.forward)) < 30f;
+                var isForwardLook = Mathf.Abs(Vector3.Angle(diff.With(y: 0), _transform.rotation * Vector3.forward)) < _searchAngle;
                 if (!(_thresholdStepHeight < diff.y) || !(diff.y < _maxStepHeight) || !isForwardLook) 
                     continue;
                 
-                _rigidbody.AddForce(Vector3.up * _force); // TODO: Add force according to the movingForce
+                _rigidbody.AddForce(Vector3.up * _jumpForce * moveAmount);
                 break;
             }
         }
@@ -34,7 +36,8 @@ namespace _Guardian_of_the_Light.Scripts.Player
 
         [SerializeField] private float _maxStepHeight = 0.5f;
         [SerializeField] private float _thresholdStepHeight = 0.1f;
-        [SerializeField] private float _force = 5000f;
+        [SerializeField] private float _searchAngle = 30f;
+        [SerializeField] private float _jumpForce = 5000f;
 
         private Rigidbody _rigidbody;
         private Transform _transform;
